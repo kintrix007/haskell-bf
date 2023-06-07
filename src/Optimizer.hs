6f12242e319e9de4bfeb273f080d9830c049354f
kintrix007 <boldizsar02@gmail.com>
2023-06-07 13:47:41 +0200

    feat: Added support for various 'Move's in the opttimizer

--- src/Optimizer.hs
+++ src/Optimizer.hs
@@ -10,6 +10,7 @@ data Command
   | Shift !Int
   | Move !Int
   | Move2 !Int !Int
+  | MoveMult !Int !Int -- ^First offset then multiplier
   | Copy !Int
   | Zero
   | ScanRFor !Int8
@@ -46,7 +47,9 @@ parseAdd :: Parser Lex [Command]
 parseAdd = do
   xs <- some (one LPlus <|> one LMinus)
   let total = sum . map value $ xs
-  return [Add total]
+  if total == 0
+    then return []
+    else return [Add total]
   where
     value LPlus = 1
     value LMinus = -1
@@ -56,7 +59,9 @@ parseShift :: Parser Lex [Command]
 parseShift = do
   xs <- some (one LLeft <|> one LRight)
   let total = sum . map value $ xs
-  return [Shift total]
+  if total == 0
+    then return []
+    else return [Shift total]
   where
     value LLeft = -1
     value LRight = 1
@@ -79,5 +84,45 @@ parseWrite = do
   _ <- one LWrite
   return [Write]
 
+parseMove :: Parser Lex [Command]
+parseMove = do
+  _ <- one LOpen
+  _ <- one LMinus
+  [Shift n] <- parseShift
+  _ <- one LPlus
+  [Shift m] <- parseShift
+  _ <- one LClose
+  if n == (-m)
+    then return [Move n]
+    else empty
+
+parseMove2 :: Parser Lex [Command]
+parseMove2 = do
+  _ <- one LOpen
+  _ <- one LMinus
+  [Shift n] <- parseShift
+  _ <- one LPlus
+  [Shift m] <- parseShift
+  _ <- one LPlus
+  [Shift k] <- parseShift
+  _ <- one LClose
+  if n == -(m + k) && n /= (-m)
+    then return [Move2 n (n+m)]
+    else empty
+
+parseMoveMult :: Parser Lex [Command]
+parseMoveMult = do
+  _ <- one LOpen
+  _ <- one LMinus
+  [Shift n] <- parseShift
+  mult <- length <$> some (one LPlus)
+  [Shift m] <- parseShift
+  _ <- one LClose
+  if n == (-m)
+    then return [MoveMult n mult]
+    else empty
+
 parseStep1 :: Parser Lex [Command]
-parseStep1 = parseAdd <|> parseShift <|> parseZero <|> parseRead <|> parseWrite <|> parseLoop
+parseStep1 =
+  parseAdd <|> parseShift <|> parseZero <|> parseRead <|> parseWrite
+  <|> parseMove2 <|> parseMove <|> parseMoveMult <|> parseLoop
