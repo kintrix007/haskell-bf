24bc4279007c1b7de6ae8ac43ab87f53d1575c18
kintrix007 <boldizsar02@gmail.com>
2023-06-07 12:12:27 +0200

    feat: Print the optimized interpretation of the program for debugging

--- src/Main.hs
+++ src/Main.hs
@@ -2,12 +2,16 @@ module Main (main) where
 
 import Lex
 import System.Environment (getArgs)
+import Optimizer
+import Parser
 
 main :: IO ()
 main = do
   [bfFilePath] <- getArgs
   bfSource <- readFile bfFilePath
   let lexes = lexBf bfSource
+  let optimized = optimize lexes
 
   print bfSource
   print lexes
+  print optimized
