03dd7fe1783b46aa3c911a37aa41dc5bd5d6e695
kintrix007 <boldizsar02@gmail.com>
2023-06-07 12:11:24 +0200

    fix: Fix incorrect export list. Did not list the constructors before

--- src/Lex.hs
+++ src/Lex.hs
@@ -1,28 +1,28 @@
-module Lex (Token, charToLex, lexBf) where
+module Lex (Lex (..), charToLex, lexBf) where
 
 import Data.Maybe (mapMaybe)
 
-data Token
-  = Plus
-  | Minus
-  | StepL
-  | StepR
-  | Open
-  | Close
-  | Read
-  | Write
+data Lex
+  = LPlus
+  | LMinus
+  | LLeft
+  | LRight
+  | LOpen
+  | LClose
+  | LRead
+  | LWrite
   deriving (Eq, Show, Enum)
 
-charToLex :: Char -> Maybe Token
-charToLex '+' = Just Plus
-charToLex '-' = Just Minus
-charToLex '<' = Just StepL
-charToLex '>' = Just StepR
-charToLex '[' = Just Open
-charToLex ']' = Just Close
-charToLex ',' = Just Read
-charToLex '.' = Just Write
+charToLex :: Char -> Maybe Lex
+charToLex '+' = Just LPlus
+charToLex '-' = Just LMinus
+charToLex '<' = Just LLeft
+charToLex '>' = Just LRight
+charToLex '[' = Just LOpen
+charToLex ']' = Just LClose
+charToLex ',' = Just LRead
+charToLex '.' = Just LWrite
 charToLex _ = Nothing
 
-lexBf :: String -> [Token]
+lexBf :: String -> [Lex]
 lexBf = mapMaybe charToLex
