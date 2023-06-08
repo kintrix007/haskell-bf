292cf8e09b0be904b7c84046c1bcf4a5ee2dc415
kintrix007 <boldizsar02@gmail.com>
2023-06-08 21:54:20 +0200

    fix: Fixed some bugs the world's slowest interpreter

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -1,10 +1,10 @@
 module Interpreter (interpret) where
 
 import Data.Array
-import Data.Word (Word8)
-import Optimizer (Command (..))
-import Debug.Trace (trace)
 import Data.Char
+import Data.Word (Word8)
+import Debug.Trace (trace)
+import Optimizer (Command (..))
 
 startingArray :: Array Int Word8
 startingArray = array bound $ zip [fst bound .. snd bound] (repeat 0)
@@ -23,21 +23,25 @@ run (Shift i) n ar
   | not $ inRange (bounds ar) n = Nothing
   | otherwise = Just (n + i, ar)
 run (Move i) n ar
+  | ar ! n == 0 = Just (n, ar)
   | not $ inRange (bounds ar) (n + i) = Nothing
-  | otherwise = Just (n, ar // [(n, 0), (n + i, ar ! n)])
+  | otherwise = Just (n, ar // [(n, 0), (n + i, (ar ! (n + i)) + (ar ! n))])
 run (Move2 i j) n ar
+  | ar ! n == 0 = Just (n, ar)
   | not $ inRange (bounds ar) (n + i) = Nothing
   | not $ inRange (bounds ar) (n + j) = Nothing
-  | otherwise = Just (n, ar // [(n, 0), (n + i, ar ! n), (n + j, ar ! n)])
+  | otherwise = Just (n, ar // [(n, 0), (n + i, ar ! (n + i) + ar ! n), (n + j, ar ! (n + j) + ar ! n)])
 run (MoveMult i d m) n ar
+  | ar ! n == 0 = Just (n, ar)
   | (ar ! n) `rem` fromIntegral d == 0 =
-    Just (n, ar // [(n, 0), (n + i, newVal)])
+    Just (n, ar // [(n, 0), (n + i, ar ! (n + i) + newVal)])
   | otherwise = run (Loop []) n ar -- aka: hang
   where
     newVal = (ar ! n) `quot` fromIntegral d * fromIntegral m
 run (Copy i) n ar
+  | ar ! n == 0 = Just (n, ar)
   | not $ inRange (bounds ar) (n + i) = Nothing
-  | otherwise = Just (n, ar // [(n + i, ar ! n)])
+  | otherwise = Just (n, ar // [(n + i, ar ! (n + i) + ar ! n)])
 run Zero n ar =
   Just (n, ar // [(n, 0)])
 run (ScanFor wo i) n ar
@@ -54,8 +58,7 @@ run (Loop coms) n ar =
       run (Loop coms) ar' n'
 run Read n ar = undefined
 run Write n ar =
-  trace [chr $ fromIntegral (ar ! n)] Just (n, ar)
-  -- Just (n, ar)
+  Just (n, ar)
 
 runLoop :: [Command] -> Int -> Array Int Word8 -> Maybe (Int, Array Int Word8)
 runLoop [] n ar = Just (n, ar)
